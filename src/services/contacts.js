6b3de06b2305001b2b0ea809a54f0e331ff9c04d
Bibosss <savoniksasha7@gmail.com>
2025-04-07 13:36:43 +0300

    hw3

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -4,3 +4,24 @@ export const getContacts = () => ContactColection.find();
 
 export const getContactId = (contactId) =>
   ContactColection.findOne({ _id: contactId });
+
+export const addContact = (payload) => ContactColection.create(payload);
+
+export const updateContact = async (_id, payload, options = {}) => {
+  const { upsert = false } = options;
+  const rawResult = await ContactColection.findOneAndUpdate({ _id }, payload, {
+    new: true,
+    upsert,
+    includeResultMetadata: true,
+  });
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    data: rawResult.value,
+    isNew: Boolean(rawResult.lastErrorObject.upserted),
+  };
+};
+
+export const deleteContactByID = (_id) =>
+  ContactColection.findByIdAndDelete(_id);
