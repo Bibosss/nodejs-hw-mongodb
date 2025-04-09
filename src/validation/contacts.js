7eec5385cdfdad8a946847f28bbcdfe79cc3fb5f
Bibosss <savoniksasha7@gmail.com>
2025-04-09 14:00:03 +0300

    fixed patch

---
+++ src/validation/contacts.js
@@ -0,0 +1,9 @@
+// import Joi from 'joi';
+
+// export const contactAddSchema = Joi.object({
+//   name: Joi.string().min(3).max(20).required(),
+//   phoneNumber: Joi.string().min(3).max(20),
+//   email: Joi.string().min(3).max(20).optional(),
+//   isFavourite: Joi.boolean().required(),
+//   contactType: Joi.string().valid('work', 'home', 'personal').required(),
+// });
